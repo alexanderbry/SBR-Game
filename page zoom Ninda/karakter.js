0de52dcdb9ba9412fecc5cbf63c472307ac21f3b
Ninda-dev <armilaninda@gmail.com>
2024-08-15 23:42:04 +0700

    data karakter

---
+++ page zoom Ninda/karakter.js
@@ -0,0 +1,29 @@
+let karakter = [
+    {
+        id : 1,
+        nama: 'Bryan',
+        desc:'',
+        Image: ''
+    },{
+        id : 2,
+        nama: 'Ninda',
+        desc:'',
+        Image: ''
+    },{
+        id : 3,
+        nama: 'Tasya',
+        desc:'',
+        Image: ''
+    },{
+        id : 4,
+        nama: 'Kevin',
+        desc:'Hai guys! AKULAH POROS DUNIAAAAA!!!!!! :D',
+        Image: ''
+    },{
+        id : 5,
+        nama: 'Amik',
+        desc:'Preman terkuat se-dunia',
+        Image: ''
+    },
+    
+]
